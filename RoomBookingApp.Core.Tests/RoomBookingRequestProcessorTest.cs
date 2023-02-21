fe1a6f52925639f72557c2473d6400a939790ee4
Abhishek <ashriv2020@gmail.com>
2023-02-21 10:04:08 +0530

    Inital project setup using xUnit, C#, .Net 7.0 and visual studio 2022

---
+++ RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
@@ -0,0 +1,35 @@
+﻿using MeetingRoomBookingApp.Core.Infrastructures;
+using MeetingRoomBookingApp.Core.Models;
+using Shouldly;
+
+namespace MeetingRoomBookingApp.Core.Tests
+{
+    public class RoomBookingRequestProcessorTest
+    {
+        [Fact]
+        public void Should_Return_Room_Booking_Response_With_Request_Values()
+        {
+            var request = new RoomBookingRequest()
+            {
+                FullName = "Test Name",
+                Email = "test@request.com",
+                Date = new DateTime(2023, 02, 19)
+            };
+            var processor = new RoomBookingRequestProcessor();
+
+            //Act
+            RoomBookingResult result = processor.BookRoom(request);
+
+            //Assert
+            Assert.NotNull(result);           
+            Assert.Equal(request.FullName, result.FullName);
+            Assert.Equal(request.Date, result.Date);
+
+            result.ShouldNotBeNull();
+            result.FullName.ShouldBe(request.FullName);
+            result.Date.ShouldBe(request.Date);
+        }
+    }
+
+    
+}
---
+++ RoomBookingApp.Core/Infrastructures/RoomBookingRequestProcessor.cs
@@ -0,0 +1,18 @@
+﻿using MeetingRoomBookingApp.Core.Models;
+
+namespace MeetingRoomBookingApp.Core.Infrastructures
+{
+    public class RoomBookingRequestProcessor
+    {
+
+        public RoomBookingResult BookRoom(RoomBookingRequest bookingRequest)
+        {
+            return new RoomBookingResult()
+            {
+                FullName = bookingRequest.FullName,
+                Date = bookingRequest.Date,
+                Email = bookingRequest.Email,
+            };
+        }
+    }
+}
