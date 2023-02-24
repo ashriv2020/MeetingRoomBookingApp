f76cb3873163c9625c3a81e659b77cf61af9e4cf
ashriv <ashriv2020@gmail.com>
2023-02-25 00:31:18 +0530

    Finish core test cases

---
+++ RoomBookingApp.Core/Contracts/IRoomBookingService.cs
@@ -0,0 +1,16 @@
+﻿using MeetingRoomBookingApp.Core.Domain;
+using MeetingRoomBookingApp.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetingRoomBookingApp.Core.Contracts
+{
+    public interface IRoomBookingService
+    {
+        void Save(RoomBooking roomBookingRequest);
+        IEnumerable<Room> GetAvailableRooms(DateTime date);
+    }
+}
